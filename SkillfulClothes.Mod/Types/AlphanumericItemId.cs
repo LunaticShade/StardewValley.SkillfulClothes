1457b690101e17667a24fb92e001952ce3baee05
LunaticShade <lunaticshade@googlemail.com>
2024-09-17 13:09:39 +0200

    remove WiP version of Editor

--- SkillfulClothes.Mod/Types/AlphanumericItemId.cs
+++ SkillfulClothes.Mod/Types/AlphanumericItemId.cs
@@ -32,5 +32,10 @@ namespace SkillfulClothes.Types
         {
             return ItemId.GetHashCode();
         }
+
+        public override string ToString()
+        {
+            return ItemName ?? ItemId;
+        }
     }
 }
